d8798d126cbe3f2144ad34b5640d569602494d8d
armcortex3445 <wlghks5966@naver.com>
2024-10-31 14:28:12 +0900

    <refactor> refactor function
    - refactor symbol of Generic type for following convention
    - refactor delay logic to be controlled by user

--- src/task/task.api.ts
+++ src/task/task.api.ts
@@ -4,16 +4,17 @@ import { promises } from "fs";
 import { initFileWrite } from "../utils/file";
 import { loadObjectFromJSON } from "../utils/jsonUtils";
 
-export function getTaskObeservable<T, R>(
-  inputList: R[],
-  apiMethod: (list: R, ...params: any[]) => Promise<T>,
+export function getTaskObeservable$<T, R>(
+  inputList: T[],
+  apiMethod: (input: T, ...params: any[]) => Promise<R>,
+  delayMiliSecond: number = 1000,
   optionalParams?: any[]
 ) {
   Logger.debug("[getTaskObeservable] : total lenth is " + inputList.length);
 
-  const task$: Observable<T> = of(...inputList).pipe(
+  const task$: Observable<R> = of(...inputList).pipe(
     tap((input) => Logger.info("[input] : " + JSON.stringify(input, null, 2))),
-    delay(1000),
+    delay(delayMiliSecond),
     concatMap((input) => apiMethod(input, optionalParams)),
     catchError((err) => {
       Logger.warn("getPaintingDetails stop." + JSON.stringify(err, null, 2));
