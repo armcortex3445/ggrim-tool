b035225f1fe82e3d8b39a502fd07cc31a1fe7d52
armcortex3445 <wlghks5966@naver.com>
2024-12-22 18:09:43 +0900

    feat(task) : add backend task

--- src/task/task.backend.ts
+++ src/task/task.backend.ts
@@ -1,26 +1,133 @@
+import { Observable, concatMap, every, of } from "rxjs";
 import {
-  IPainting,
-  IResult,
-  WikiArtPainting,
-  getPaintingDTO,
-  getPaintingFromDB,
-} from "../api/back-server/api";
-import {
-  SearchPaintingDTO,
-} from "../api/back-server/dto";
-import {
-  BackendArtist,
-  BackendPainting,
   ExtendedBackendPainting,
   IPaginationResult,
 } from "../api/back-server/type";
 import { Painting } from "../api/wikiArt/interfaces";
 import { Logger } from "../utils/logger";
 import {
+  IRestAPITest,
   getTaskForRestAPITest$,
   getTaskForValidateRestAPI$,
 } from "./task.test.api";
 import { validatePaintingFromDB } from "./backend/validation";
+import { initFileWrite } from "../utils/file";
+import { appendFileSync } from "fs";
+import { loadListFromJSON } from "../utils/jsonUtils";
+import {
+  getExtendedBackendPaintingByPainting,
+  insertArtistWhenNotExisted,
+  insertPaintingWhenNotExist,
+  insertStyleWhenNotExist,
+  insertTagWhenNotExist,
+} from "./backend/api";
+
+export function runInsertPaintingStepByStep(paintingFile: string) {
+  /*로직 설계
+  1. [x]작가 존재확인 및 삽입
+  2. [x]태그 존재 확인 및 삽입
+  3. [x]스타일 존재 확인 및 삽입
+  4. [x]그림 존재 확인 및 삽입
+  5. []테스트 진행
+  6. []수행 검증 결과 로직 적용
+  7
+  */
+
+  const paintings: Painting[] = loadListFromJSON<Painting>(
+    paintingFile,
+    undefined
+  );
+  const restAPITests: IRestAPITest<Painting, string>[] = paintings.map(
+    (painting) => {
+      const restAPITest: IRestAPITest<Painting, string> = {
+        local: painting,
+        apiResult: `${painting.id}`,
+      };
+      return restAPITest;
+    }
+  );
+
+  let taskLogFileName = "[task]insertPainting.log.txt";
+  let taskResultFileName = "[task]insertPainting.result.txt";
+
+  taskLogFileName = initFileWrite(
+    taskLogFileName,
+    `#inputPath=${paintingFile}\n`,
+    "utf-8"
+  );
+  taskResultFileName = initFileWrite(
+    taskResultFileName,
+    `#inputPath=${paintingFile}\n`,
+    "utf-8"
+  );
+
+  const task$: Observable<IRestAPITest<Painting, string>> = of(
+    ...restAPITests
+  ).pipe(
+    concatMap(async (restAPITest) => {
+      const painting = restAPITest.local;
+      const result: string = await insertArtistWhenNotExisted(
+        painting.artistName
+      );
+
+      if (result.length !== 0) {
+        restAPITest.apiResult += "\n\t";
+        restAPITest.apiResult += result;
+      }
+      return restAPITest;
+    }),
+    concatMap(async (restAPITest) => {
+      const painting = restAPITest.local;
+      restAPITest.apiResult += "\n\t#Insert Tag";
+      for (const tag of painting.tags) {
+        const result: string = await insertTagWhenNotExist(tag);
+        if (result.length !== 0) {
+          restAPITest.apiResult += "\n\t\t";
+          restAPITest.apiResult += result;
+        }
+      }
+
+      return restAPITest;
+    }),
+    concatMap(async (restAPITest) => {
+      const painting = restAPITest.local;
+      restAPITest.apiResult += "\n\t#Insert Style";
+      for (const style of painting.styles) {
+        const result: string = await insertStyleWhenNotExist(style);
+        if (result.length !== 0) {
+          restAPITest.apiResult += "\n\t\t";
+          restAPITest.apiResult += result;
+        }
+      }
+      return restAPITest;
+    }),
+    concatMap(async (restAPITest) => {
+      const painting = restAPITest.local;
+      const result = await insertPaintingWhenNotExist(painting);
+
+      if (result.length !== 0) {
+        restAPITest.apiResult += "\n\t";
+        restAPITest.apiResult += result;
+      }
+
+      return restAPITest;
+    })
+  );
+
+  task$.subscribe(async (restAPITest) => {
+    const painting = restAPITest.local;
+    appendFileSync(
+      taskLogFileName,
+      (restAPITest.apiResult || `${painting.id} has problem`) + "\n"
+    );
+
+    const createdData: IPaginationResult<ExtendedBackendPainting> =
+      await getExtendedBackendPaintingByPainting(painting);
+
+    const taskResult = validatePaintingFromDB(painting, createdData);
+    appendFileSync(taskResultFileName, `${painting.id}${taskResult}\n`);
+  });
+}
 
 export function testGetPaintingAPI(paintings: Painting[]) {
   Logger.info("[testGetPaintingAPI] start");
